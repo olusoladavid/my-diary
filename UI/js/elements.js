65179dc9094b63c770a1a90c120d1296aad68625
Olusola David <sola.dav7@gmail.com>
2018-07-12 15:34:03 +0100

    [Feature #158889360] Add edit and delete button interactions

--- UI/js/elements.js
+++ UI/js/elements.js
@@ -42,3 +42,59 @@ const initApp = (e) => {
 };
 
 document.addEventListener('DOMContentLoaded', initApp);
+
+/* story functions */
+
+let editMode = false;
+
+try {
+	const _bar = document.querySelector(".form-delete");
+	const _bar2 = document.querySelector(".form-Not");
+	const modal_content = document.querySelector("div.modal_content");
+	const close_button = document.querySelector(".form-sure");
+	const _like =document.querySelector(".icon .heart");
+	const _edit = document.querySelector(".form-sub");
+	const close_menu = document.querySelector('.close-menu');
+
+	_bar.addEventListener('click', () => {
+		modal_content.style.display ='block';
+	});
+	_bar2.addEventListener('click', () => {
+		modal_content.style.display ='none';
+	});
+
+//closing the modal_content
+close_button.addEventListener('click', () =>{
+	modal_content.style.display ='none';
+});
+
+_like.addEventListener('click', () => {
+	_like.style.color ='red';
+	_like.classList.add("scale");
+});
+
+_edit.addEventListener('click', (e) => {
+	const el = document.getElementById("full-story-content");
+	if (!editMode) {
+		const range = document.createRange();
+		const sel = window.getSelection();
+		range.setStart(el.childNodes[0], 0);
+		range.collapse(true);
+		sel.removeAllRanges();
+		sel.addRange(range);
+		el.focus();
+		e.target.innerHTML = "Save";
+		editMode = true;
+	}
+	else {
+		/* perform fetch operation */
+		e.target.innerHTML = "Edit";
+		editMode = false;
+		window.focus();
+	}
+});
+}
+
+catch(err) {
+	// Story not available
+}
