bebc0c498b2865b87f3f7bbd17151e8bad1a513c
Olusola David <sola.dav7@gmail.com>
2018-07-20 16:32:00 +0100

    [Feature #159068545] Add /DELETE/:id entries endpoint

---
+++ server/controllers/deleteEntry.js
@@ -0,0 +1,13 @@
+import entries from '../db/entries';
+
+const deleteEntry = (req, res) => {
+  const indexOfFound = entries.findIndex(entry => entry.id === Number(req.params.id));
+  if (indexOfFound > -1) {
+    entries.splice(indexOfFound, 1);
+    res.status(204).json();
+  } else {
+    res.status(404).json({ errors: [{ msg: 'Entry does not exist' }] });
+  }
+};
+
+export default deleteEntry;
