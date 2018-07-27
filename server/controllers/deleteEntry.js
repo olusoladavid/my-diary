bf216e4fb7856f0a2989483285ee63c6a1959855
Olusola David <sola.dav7@gmail.com>
2018-07-27 02:35:14 +0100

    feat(API): Refactor DELETE /entries/:id to use database
    - Create SQL queries
    - Refactor route to delete entry from database
    
    [Starts #159328809]

--- server/controllers/deleteEntry.js
+++ server/controllers/deleteEntry.js
@@ -1,13 +1,34 @@
-import { entries } from '../db/entries';
+import pg from 'pg';
 
 const deleteEntry = (req, res) => {
-  const indexOfFound = entries.findIndex(entry => entry.id === Number(req.params.id));
-  if (indexOfFound > -1) {
-    entries.splice(indexOfFound, 1);
-    res.status(204).json();
-  } else {
-    res.status(404).json({ errors: [{ msg: 'Entry does not exist' }] });
-  }
+  const config = {
+    user: 'postgres',
+    database: 'mydiary',
+    password: 'postgres',
+    port: 5432,
+  };
+  const pool = new pg.Pool(config);
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.log(`not able to get connection ${err}`);
+      res.status(500).send(err);
+    }
+    client.query(`DELETE FROM entries 
+    USING users 
+    WHERE entries.user_id=users.id 
+    AND users.email='user@gmail.com' 
+    AND entries.id=${req.params.id}`, (queryError, result) => {
+      done(); // closing the connection;
+      if (queryError) {
+        console.log(queryError);
+        return res.status(500).send(queryError);
+      }
+      if (!result.rowCount) {
+        return res.status(404).json({ message: 'Not found' });
+      }
+      return res.status(204).json();
+    });
+  });
 };
 
 export default deleteEntry;
