adc08e57a29472c90c0771831c35bd7e39e5c0de
Olusola David <sola.dav7@gmail.com>
2018-07-27 02:04:35 +0100

    feat(API): Add database function to GET /entries
    - Create SQL queries
    - Refactor route to retrieve data from database
    
    [Partially delivers #159352571]

--- server/controllers/getAllEntries.js
+++ server/controllers/getAllEntries.js
@@ -1,7 +1,31 @@
-import { entries } from '../db/entries';
+import pg from 'pg';
 
 const getAllEntries = (req, res) => {
-  res.status(200).json(entries);
+  const config = {
+    user: 'postgres',
+    database: 'mydiary',
+    password: 'postgres',
+    port: 5432,
+  };
+  const pool = new pg.Pool(config);
+  pool.connect((err, client, done) => {
+    if (err) {
+      console.log(`not able to get connection ${err}`);
+      res.status(500).send(err);
+    }
+    client.query(`SELECT entries.id, entries.title, entries.content, entries.created_on, entries.is_favorite, 
+    entries.is_public FROM entries 
+    INNER JOIN users 
+    ON entries.user_id = users.id 
+    WHERE users.email='user@gmail.com'`, (queryError, result) => {
+      done(); // closing the connection;
+      if (queryError) {
+        console.log(queryError);
+        res.status(400).send(queryError);
+      }
+      res.status(200).send(result.rows);
+    });
+  });
 };
 
 export default getAllEntries;
